83df4133b419fda4d416d361ba49016ccca24285
vonzhou <vonzhou@163.com>
2015-11-07 20:35:34 +0800

    dump to a file

--- sum_new.py
+++ sum_new.py
@@ -6,6 +6,7 @@ such as shunfeng.txt
 """
 
 import sys
+
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
@@ -24,9 +25,10 @@ exp_set = set(exp_list)
 for exp in exp_list:
 	# print exp
 	temp = open(exp, 'w')
-	temp.seek(0)
 	exp_files[exp] = temp
 
+multilist = [[0 for col in range(200)] for row in range(30)]	
+areas_list = [0 for col in range(200)]
 
 f = open('dataset.txt', 'r')
 for line in f:
@@ -44,41 +46,38 @@ for line in f:
 		# print exp_name
 		if exp_name not in exp_set:
 			exp_set.add(exp_name)
-			temp = open(exp_name, 'w')
-			temp.seek(0)
-			exp_files[exp_name] = temp
-
+			#exp_files[exp_name] = open(exp_name, 'w')
+		lie = exp_set.index(expname)
 		# print 'write line to ', exp_name 
-		fi = exp_files[exp_name]
+		#fi = exp_files[exp_name]
 
-		areas = line[(k+1):-1]
-		# if flag:
-		# 	fi.write('\t\t'+ current_city + ':\n')
-		# 	flag = False
-		# print '|||'+areas
-		# area_list = areas.split(',')
-		fi.write('\t\t' + areas + '\n')
-
-
-for afile in exp_files.values():
-	afile.flush()
-
-for afile in exp_files.values():
-	afile.close()
-
-out = open('sum.txt', 'w')
-exp_files2 = dict()
-for afilename in exp_set:
-	afile = open(afilename, 'r')
-	exp_files2[afilename] = afile
-	out.write(afile.name + ':\n')
-	for line in afile:
-		# print 'line:', line
-		out.write('\t'+line)
-out.flush()
-out.close()
-
-for afile in exp_files2.values():
-	afile.close()
+		areas = line[(k+1):]
+		newstr = areas
+		kk = newstr.find("(")
+		while kk >= 0:
+			area = newstr[:kk-1]
+			if area not in area_list:
+				area_list.add(area)
+			hang = area_list.index(area)
+			newstr = newstr[newstr.find("(")+1:]    #这样一致到newstr里面不包含a为止
+			kkk = newstr.find(")")
+			multilist[hang][lie] = newstr[:kkk-1]
+			newstr = newstr[newstr.find(",")+1:]    
+		#area_list = areas.split(',')
+		#for a in area_list:
+			# print a
+		#	fi.write(current_city + '-' + areas + '\n')
+print("	")
+for a in exp_list:
+	print(a, end="	")
+i = 0	
+for b in area_list:
+	print(b, end="	")
+	for j in rang(0,29):
+		print(multilist[i][j], end="	")
+	print("\n")
+	i=i+1
+#for f in exp_files.values():
+#	f.close()
 
 
